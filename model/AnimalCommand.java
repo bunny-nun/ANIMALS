a466873e8b739f6dcf897773e4dd35b78f1e690a
Valeria <nelera.gb@gmail.com>
2023-09-17 18:41:17 +0300

    class AnimalCommand added to the model

---
+++ model/AnimalCommand.java
@@ -0,0 +1,27 @@
+package model;
+
+public class AnimalCommand {
+    int commandID;
+    String description;
+
+    public AnimalCommand(int commandID, String description) {
+        this.commandID = commandID;
+        this.description = description;
+    }
+
+    public int getCommandID() {
+        return commandID;
+    }
+
+    public void setCommandID(int commandID) {
+        this.commandID = commandID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
