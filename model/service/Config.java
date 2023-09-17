2e2e2a958efb5fd84cf66571032e8d7eeb0fc730
Valeria <nelera.gb@gmail.com>
2023-09-17 18:40:17 +0300

    configuration-file for db-connection added

---
+++ model/service/Config.java
@@ -0,0 +1,9 @@
+package model.service;
+
+public class Config {
+    // файл не включен в gitignore, чтобы можно было проверить подключение к БД
+    public static String HOST = "jdbc:mysql://217.151.230.50:3306/human_friends";
+    public static String USER = "bunny";
+    public static String PASSWORD = "123";
+
+}
